f9af1c6802a49aca8f70806be38703c2077e5c91
Isaac cruz <isaaccarrico5912@gmail.com>
2024-05-16 11:07:27 -0300

    Add files via upload

---
+++ pisonho.js
@@ -0,0 +1,5 @@
+let x = -1
+let caculadoraSoma = x + 1
+let array = ["voce iluminado", "thiago e otario", "o amor vence tudo"]
+let texto = 'as vezes voce tem que bater a cara pra apender '
+console.log(caculadoraSoma)
