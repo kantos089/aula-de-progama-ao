2cbccd7a76453df0ccb37d31ea8d9b3858251d60
Isaac cruz <isaaccarrico5912@gmail.com>
2024-06-12 12:30:51 -0300

    Aula de objeto

--- tipo.js
+++ tipo.js
@@ -1,4 +1,10 @@
 let fruta = "maçã "
 let frutas = [" banana", "marango", "amora" ]
-let banana = {}
-console.log(fruta + frutas)
+let banana = {
+    "cor": "amalera",
+    "sabor": "doce",
+    "leguminosa": "não",
+    "fruta": "sim"
+}
+
+console.log("A cor da banana é: " + banana.cor +  " O sabor da banana é: " + banana.sabor + " A fruta banana é: " + banana.fruta + " A leguminosa banana é: " + banana.leguminosa)
